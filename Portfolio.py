930ddec86b969295c60a55ac6af53902f50e0127
Michael Perez <mperez1498@gmail.com>
2024-01-12 13:07:35 -0500

    Added the new files and cleaned up my tab

---
+++ Portfolio.py
@@ -0,0 +1,50 @@
+class Portfolio(Time_Series):
+
+    def __init__(self, stocks  = [], options = []):
+        self.stocks = stocks
+        self.options = options
+        self.portfolio_of_assets = {"Stock": self.stocks, "Option": self.options}
+        self.value = self.generate_combined_value()
+
+    def generate_combined_value(self):
+        cumulative_value = 0
+        for key, value in self.portfolio_of_assets.items():
+            for items in value:
+                cumulative_value = items.value
+
+
+        return result_dict
+
+    def __setattr__(self, value_replacing, value_to_be_replaced):
+
+        if isinstance(value_replacing, Stock):
+            print("Setting the Stocks in our portfolio to a new list of stocks. ")
+        elif isinstance(value_replacing, Option):
+            print("Setting the Options in portfolio to a new list of options.")
+        elif isinstance(value_replacing, Portfolio):
+            print("Setting the Portfolio to a new Portfolio")
+        else:
+            print('Incompatible Value')
+            return ValueError
+
+        super().__setattr__(value_replacing, value_to_be_replaced)
+
+    def __iter__(self):
+        self.current_index = 0
+        return self
+
+    def __add__(self, asset_to_include):
+
+        if isinstance(asset_to_include, Stock):
+            new_portfolio = {'Stock': self.portfolio_of_assets['Stock'].append(asset_to_include),
+                             'Option': self.portfolio_of_assets['Option']}
+            return Portfolio(stocks = new_portfolio['Stock'], options = new_portfolio['Option'])
+
+        elif isinstance(asset_to_include, Option):
+            new_portfolio = {'Stock': self.portfolio_of_assets['Stock'],
+                             'Option': self.portfolio_of_assets['Option'].append(asset_to_include)}
+            return Portfolio(stocks = new_portfolio['Stock'], options = new_portfolio['Option'])
+        elif isinstance(asset_to_include, Portfolio):
+            new_portfolio = {'Stock':  self.portfolio_of_assets['Stock'].append(asset_to_include.portfolio_of_assets['Stock']),
+                             'Option': self.portfolio_of_assets['Option'].append(asset_to_include.portfolio_of_assets['Option'])}
+            return Portfolio(stocks = new_portfolio['Stock'], options = new_portfolio['Option'])
---
+++ Utilities.py
@@ -0,0 +1,30 @@
+import polygon
+import plotly
+import plotly.express as px
+from polygon import build_polygon_option_symbol
+from polygon.enums import OptionSymbolFormat
+import requests
+import pandas as pd
+import yfinance as yf
+import datetime
+from datetime import date
+from datetime import timedelta
+
+apiKey = "oIpqVdgJYK9nldqQ5j8JiPXxVVZptz0a"
+payload = {}
+headers = {
+    'Accept': 'text/json',
+    'Sec-Fetch-Site': 'same-site',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Sec-Fetch-Mode': 'cors',
+    'Host': 'api.polygon.io',
+    'Origin': 'https://polygon.io',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15',
+    'Referer': 'https://polygon.io/',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'empty',
+    'X-Polygon-Edge-User-Agent': 'useragent',
+    'X-Polygon-Edge-IP-Address': '8.8.8.8',
+    'X-Polygon-Edge-ID': 'sample_edge_id'
+}
